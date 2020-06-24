b7508ed55f55234bdf606963f4006badc222253e
Travis G Calley <tgc1002@wildcats.unh.edu>
2020-06-24 18:07:55 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import travis from './images/portfolio-pic.jpeg';
+import './App.css'
+
+function MainBody() {
+  return (
+    <div className="Main">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+         sed do eiusmod tempor incididunt ut labore et dolore magna 
+         aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+         ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+         Duis aute irure dolor in reprehenderit in voluptate velit 
+         esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+         occaecat cupidatat non proident, sunt in culpa qui officia 
+         deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="Headshot" 
+           src={travis}
+           alt="Travis Calley professional headshot"/>
+      <MainBody />
     </div>
+   
   );
 }
 
